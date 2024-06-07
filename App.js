43cb3fe17298e528c0825f90193a17cd8917c012
Bharat <bharatdhiman40@gmail.com>
2024-06-07 20:24:02 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
-/**
- * <div id="parent-container">
- *     <div id="first-child-container">
- *          <h1 id="heading1">I am h1 tag</h1>
- *          <h2 id="heading2">I am h2 tag</h2>
- *      </div>
- *     <div id="second-child-container">
- *          <h1 id="heading1">I am h1 tag</h1>
- *          <h2 id="heading2">I am h2 tag</h2>
- *      </div>
- * </div>
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// for displaying anything onto the browser we use ReactDOM
 
-const parentDiv = React.createElement("div", { id: "parent-container" }, [
-    React.createElement("div", { id: "first-child-container" }, [
-        React.createElement("h1", { id: "heading1" }, "This is namaste react"),
-        React.createElement("h2", { id: "heading2" }, "I am h2 tag")
-    ]
-    ),
-    React.createElement("div", { id: "second-child-container" }, [
-        React.createElement("h3", { id: "heading3" }, "I am h3 tag"),
-        React.createElement("h4", { id: "heading4" }, "I am h4 tag")
-    ]
-    )]
-);
+
+
+// React.createElement() -> React Element -> JS object -> when we render this element onto DOM this becomes HTML element 
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading-1" },
+//     "Namaste React 🚀"
+// );
+
+
+
+
+// JSX -> is NOT HTML in JS -> it is HTML like syntax
+const jsxHeading = <h1 className="head" tableIndex="5">Namaste React 🚀</h1>
+// JSX -> babel in parcel transpile it into React.createElement() -> React Element -> JS object -> when we render this element onto DOM this becomes HTML element 
+
+// React component
+// class based component - OLD
+// functional component - NEW
+
+// ? what is a React functional component?
+// just a normal JavaScript function which returns some normal piece of JSX
+// ! name it with a CAPITAL letter, otherwise it will give error
+
+// below is example
+const TitleComponent = () => {
+    return <h1 className="head">Namaste React 💻🚀</h1>
+}
+const ParaComponent = () => {
+    return <p className="para"> Hello I'm Bharat Dhiman <br /> MERN stack developer</p>
+}
+
+// ! can inject any JavaScript code in JSX using {javascript code}
+const data = 10000;
+const HeadingComponent = () => {
+    return <div id="container">
+        <TitleComponent />
+        <ParaComponent />
+        <h1>Namaste React using Functional Component</h1>
+    </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parentDiv);
+root.render(<HeadingComponent />);
