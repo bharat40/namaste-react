a3003744bc41d3d36682902c55a46288ad01fafe
Bharat <bharatdhiman40@gmail.com>
2024-06-08 14:02:52 +0530

    Episode-04 (Part-01)

--- App.js
+++ App.js
@@ -28,22 +28,115 @@ const jsxHeading = <h1 className="head" tableIndex="5">Namaste React 🚀</h1>
 // ! name it with a CAPITAL letter, otherwise it will give error
 
 // below is example
-const TitleComponent = () => {
-    return <h1 className="head">Namaste React 💻🚀</h1>
-}
-const ParaComponent = () => {
-    return <p className="para"> Hello I'm Bharat Dhiman <br /> MERN stack developer</p>
-}
+// const TitleComponent = () => {
+//     return <h1 className="head">Namaste React 💻🚀</h1>
+// }
+// const ParaComponent = () => {
+//     return <p className="para"> Hello I'm Bharat Dhiman <br /> MERN stack developer</p>
+// }
 
 // ! can inject any JavaScript code in JSX using {javascript code}
-const data = 10000;
-const HeadingComponent = () => {
-    return <div id="container">
-        <TitleComponent />
-        <ParaComponent />
-        <h1>Namaste React using Functional Component</h1>
+// const data = 10000;
+// const HeadingComponent = () => {
+//     return <div id="container">
+//         <TitleComponent />
+//         <ParaComponent />
+//         <h1>Namaste React using Functional Component</h1>
+//     </div>
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent />);
+
+
+
+
+/**
+ * ! Header
+ *      * Logo
+ *      * Nav Items
+ * ! Body
+ *      * Search bar
+ *      * Restaurant container
+ *      * Restaurant card
+ *          * Image
+ *          * Name
+ *          * Star rating
+ *          * Cuisine
+ *          * Delivery time
+ *  ! Footer
+ *      * Copyright
+ *      * Links
+ *      * Address
+ *      * Contact
+ */
+
+
+const Header = () => {
+    return (<div className="header">
+        <div className="logo-container">
+            <img src="https://png.pngtree.com/template/20200610/ourmid/pngtree-food-delivery-logo-design-image_381319.jpg" className="logo" />
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
     </div>
+    )
 }
 
+const RestaurantCard = () => {
+    return (
+        <div className="restaurant-card">
+            <a href="https://bharat40.github.io/FOODNOOK-restaurant/">
+                <img alt="res-logo" className="restaurant-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ezesxd6ovt0obvosm3nv" />
+                <h3>FoodNook</h3>
+                <h5>Pizza, Noodles, Biryani</h5>
+                <h5>4.5 Starts</h5>
+                <h5>30 minutes</h5>
+            </a>
+        </div >
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                search
+            </div>
+            <div className="restaurant-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (<div className="app">
+        <Header />
+        <Body />
+    </div>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
